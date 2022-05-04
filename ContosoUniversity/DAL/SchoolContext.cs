ec39609a692eb1da4f7654a9e353700766d18f8b
Derrick Nazareth <derekvenom@gmail.com>
2022-05-04 11:38:31 +0930

    Added code to database context; Seeded database with test data (Configuration File)

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -17,10 +17,19 @@ namespace ContosoUniversity.DAL
         public DbSet<Student> Students { get; set; }       //creates a DbSet property for each entity set. In Entity Framework terminology, an entity set typically corresponds to a database table, and an entity corresponds to a row in the table.
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();  //prevents table names from being pluralized. If you didn't do this, the generated tables in the database would be named Students, Courses, and Enrollments
+
+            modelBuilder.Entity<Course>()                   //Configures the many-to-many join table
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                .MapRightKey("InstructorID")
+                .ToTable("CourseInstructor"));
         }
     }
 }
